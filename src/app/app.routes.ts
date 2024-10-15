de0d4c048419d5546ba045a880187577a1e63450
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-15 07:56:36 -0300

    desenvolver rotas com angular com menu responsivo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { ConsultaComponent } from './pages/consulta/consulta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent },
+    {path: 'sobre', component: SobreComponent },
+    {path: 'cadastro', component: CadastroComponent },
+    {path: 'consulta', component: ConsultaComponent }
+];
--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
 
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
