ff3e6aa35f4c94f7963d3994e9d618cacd293d54
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-15 13:33:34 -0300

    desenvolvimento da outra pagina sobre e com imagem, e tudo fucnionando, falat o cadastro e consulta

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-sobre',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './sobre.component.html',
   styleUrl: './sobre.component.css'
 })
 export class SobreComponent {
+
+  imagemSrc = 'https://www.opensoft.pt/wp-content/uploads/2020/06/tendencias_software.png'
+
+  imagemAt = 'desenvolvimento de sistemas de software'
 
   onButtonClick() {
     alert('TI esta em alta, quanto mais estuda mais, vantagem de conhecimento temos, e podemos evoluir muito rapido.');
